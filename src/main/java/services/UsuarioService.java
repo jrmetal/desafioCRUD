48e9b6948e6cbdcad6b147541f07cb155c6073ce
Juliano Razzo <jrmetal@hotmail.com>
2018-03-05 16:05:43 +0000

    Cricação do serviço buscar por ID e Importado a Bibliteca GSON
    Tipo: M (C-Correção, I-Implementação, M-Melhoria)
    
    Descrição:
      - Criado o serviço para buscar Usuário pelo ID, e adicionado a
        dependência GSON
    
    Observações:
    
    Referências:
       - http://mvnrepository.com/artifact/com.google.code.gson/gson/2.8.2
       - https://github.com/google/gson

--- src/main/java/services/UsuarioService.java
+++ src/main/java/services/UsuarioService.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import com.google.gson.Gson;
 
 import dao.UsuarioDAO;
 import domain.Usuario;
@@ -27,10 +30,29 @@ public class UsuarioService {
 		// usuarioDAO.salvar(usuario);
 
 		List<Usuario> usuarios = usuarioDAO.listar();
-		usuario = usuarioDAO.buscarPorCodigo(usuarios.get(0).getId());
-		usuarioDAO.excluir(usuario);
-		return "Teste Restful Usuariossss" + usuario;
+		// usuario = usuarioDAO.buscarPorCodigo(usuarios.get(0).getId());
+		// usuarioDAO.excluir(usuario);
 
+		Gson gson = new Gson();
+
+		String json = gson.toJson(usuarios);
+
+		return "Teste Restful Usuariossss" + json;
+	}
+
+	@GET
+	@Path("{id}")
+	public String buscar(@PathParam("id") int id) {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+		Usuario usuario = new Usuario();
+		usuario = usuarioDAO.buscarPorCodigo(id);
+
+		Gson gson = new Gson();
+
+		String json = gson.toJson(usuario); 
+
+		return "Teste Restful Usuariossss" + json;
 	}
 
 }
