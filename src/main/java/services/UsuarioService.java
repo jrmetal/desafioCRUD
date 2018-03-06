8c5fd4f69c99071058152ef8c62a0311c7796506
Juliano Razzo <jrmetal@hotmail.com>
2018-03-06 15:40:48 +0000

    Criação dos Serviços de Inclusão, Edição e Deletar
    Tipo: I (C-Correção, I-Implementação, M-Melhoria)
    
    Descrição:
      - Criado os serviços de deletar, adicionar e editar a entidade
        Usuários
    
    Observações:
    
    Referências:

--- src/main/java/services/UsuarioService.java
+++ src/main/java/services/UsuarioService.java
@@ -3,7 +3,10 @@ package services;
 import java.util.Date;
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -50,9 +53,48 @@ public class UsuarioService {
 
 		Gson gson = new Gson();
 
-		String json = gson.toJson(usuario); 
+		String json = gson.toJson(usuario);
 
 		return "Teste Restful Usuariossss" + json;
 	}
+
+	@POST
+	public String salvar(String json) {
+
+		Gson gson = new Gson();
+		Usuario usuario = gson.fromJson(json, Usuario.class);
+
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuarioDAO.salvar(usuario);
+
+		return "{\"Status\":true,\"MSG\":\"Usuário cadastrado com sucesso\",\"ID\":" 
+		       + usuario.getId() + "}";
+	}
+	
+	@PUT
+	public String editar(String json) {
+		Gson gson = new Gson();
+		Usuario usuario = gson.fromJson(json, Usuario.class);
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuarioDAO.editar(usuario);
+		return "teste";
+	} 
+	
+	
+	@DELETE
+	public String excluir(String json){
+		
+		Gson gson = new Gson();
+		Usuario usuario = gson.fromJson(json, Usuario.class);
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuarioDAO.excluir(usuario);
+		
+		return "teste";
+		
+	}
+	
+	
 
 }
